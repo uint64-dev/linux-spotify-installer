cca5a01d42550679c82ddda3d3ecbd61e26efdcf
uint64-dev <121733684+uint64-dev@users.noreply.github.com>
2022-12-31 23:25:05 +0100

    Release 1.0

---
+++ spotify-installer.sh
@@ -0,0 +1,4 @@
+clear
+echo "This installer will download Spotify on your Linux-based PC. Please ensure you have 'snap' installed. If you do, click Enter to continue."
+read input
+snap install spotify
